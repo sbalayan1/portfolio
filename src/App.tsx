cfa091547a71b7b711c42876302bb51506fd0610
sbalayan1 <balayans2014@yahoo.com>
2023-04-09 00:14:39 -0700

    create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import NavBar from './Components/NavBar';
+import { SiJavascript, SiTypescript, SiReact, SiRubyonrails, SiPython, SiPostgresql, SiHtml5 } from 'react-icons/si'
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+  const [dark, setDark] = useState(false) 
+  const skills = [<SiJavascript className="text-yellow-500 bg-black"/>, <SiTypescript className="text-blue-700 bg-black"/>, <SiReact className="bg-black text-blue-500"/>, <SiRubyonrails className='bg-red-500'/>, <SiPython className="bg-blue-500"/>, <SiPostgresql className="bg-blue-300"/>, <SiHtml5 className="bg-orange-400" /> ]
+
+  const handleDark = () => {
+    setDark(dark => !dark)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={`text-4xl min-h-screen ${dark ? "bg-blue-950 text-black" : "bg-white text-white"}`}>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +26,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <NavBar dark={dark} handleDark={handleDark}/>
+      Hey is this working?
+      <div className='flex'>{skills.map(skill => skill)}</div>
     </div>
   );
 }
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import {MdDarkMode, MdOutlineDarkMode} from 'react-icons/md'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+// import {Link} from 'react-dpom'
+interface NavBarProps {
+    dark: boolean
+    handleDark(): void
+}
+
+export default function NavBar({dark, handleDark}: NavBarProps) {
+    const [btnsOn, setBtnsOn] = useState(false)
+    const homeBtns = ["Home", "About", "Portfolio", "Skills", "Contact" ].map((link, idx) => ({id: idx+1, link: link }))
+    const handleClick = () => {}
+    const btnsToDisplay = homeBtns.map(({id, link}) => (
+        <li 
+            key={id}
+            onClick={() => {}} 
+            className="px-5 cursor-pointer capitalize hover:scale-110 duration-200"> {link} 
+        </li>
+    ))
+
+    const displayNavBtns = () => {setBtnsOn(btnsOn => !btnsOn)}
+
+    return (
+        <div className={`flex justify-between items-center min-w-full h-20 fixed px-4 text-2xl
+                ${dark ? "bg-white text-blue-950" : "bg-blue-900 text-white"}
+                `
+            }
+        >
+            <h1 className="ml-2 cursor-pointer hover:scale-110 duration-200 ">sean_balayan.io</h1>
+            <div className="flex">
+                <ul className="hidden items-center text-lg md:flex">
+                    {homeBtns.map(({id, link}) => (
+                        <li 
+                            key={id}
+                            onClick={typeof link !== "string" ? handleDark : handleClick} 
+                            className="px-5 cursor-pointer capitalize hover:scale-110 duration-200"> {link} 
+                        </li>
+                    ))}
+                </ul>
+                <div className="mr-2 hover:scale-110 duration-200" onClick={handleDark}>
+                    {dark ? <MdDarkMode /> : <MdOutlineDarkMode/>}
+                </div>
+                <div className="px-4 cursor-pointer hover:scale-110 duration-200 z-10 md:hidden" onClick={displayNavBtns}>
+                    {btnsOn ? <FaTimes /> : <FaBars />}
+                </div>
+            </div>
+            {btnsOn &&
+                <ul className={`
+                        flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen 
+                        ${dark ? "bg-gradient-to-b from-blue-100 to-blue-950" : "bg-gradient-to-b from-blue-900 to-blue-200 "}
+                        md:hidden
+                    `} 
+                >
+                    {btnsToDisplay}
+                </ul>
+            }
+        </div>
+
+    )
+}
