96b7771da28f102f843dbaa5e02122d66fb8a3ff
sbalayan1 <balayans2014@yahoo.com>
2023-06-27 20:40:43 -0700

    test styling update

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -6,23 +6,23 @@ interface AboutProps {
 
 export default function About({dark}: AboutProps) {
     return (
-        <div id="About" className='w-screen flex flex-col items-center justifiy-center border-t text-base md:h-screen'>
+        <div id="About" className='w-screen flex flex-col items-center justifiy-center border-t text-lg md:h-screen'>
             <div className='max-w-screen-sm p-4 mx-auto w-3/4 h-full md:text-base lg:text-2xl flex flex-col justify-center'>
                 <div className='pb-4'>
                     <h2 className="font-bold inline">About</h2>
                 </div>
 
-                <p className='text-xs lg:text-sm'>
+                <p className='text-base lg:text-sm'>
                         Hello there! I am a junior web developer based in Honolulu, Hawaii, and currently employed as a Technical Coach at Flatiron School. My role involves working with and teaching students about JavaScript, React, Ruby, and Python, along with the frameworks and libraries associated with them.
                 </p>
                 <br/>
 
-                <p className='text-xs lg:text-sm'>
+                <p className='text-base lg:text-sm'>
                     As a tech enthusiast and creative problem solver, I am always eager to take on new challenges. With a natural talent for logical thinking and a passion for innovation, I am a self-motivated learner who constantly strives to expand my knowledge and skills. My keen eye for detail and knack for devising elegant solutions to complex problems have proven to be valuable assets in my work as a developer and technical coach.
                 </p>
                 <br/>
 
-                <p className='text-xs lg:text-sm'>
+                <p className='text-base lg:text-sm'>
                     Whether I am coding, cooking, or dancing, I approach every endeavor with a can-do attitude and a commitment to excellence. I believe that the combination of technical expertise and a creative mindset are key to success in any field, and I am excited to continue growing and developing as a web developer.
                 </p>
             </div>
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -12,13 +12,13 @@ export default function Home({dark, handleDark}: HomeProps) {
         <div id="Home"className="max-w-screen-sm h-screen flex justify-center items-center">
             <div className="max-w-screen-lg flex items-center justify-center px-4 md:flex-row md:h-1/2 lg:h-full">
                 <div className="flex flex-col justify-center h-full mt-8">
-                    <h2 className='text-base font-bold md:text-base lg:text-2xl'>
+                    <h2 className='text-lg font-bold lg:text-2xl'>
                         Full Stack Software Engineer
                     </h2>
-                    <p className='py-4 text-xs mr-2'>
+                    <p className='py-4 text-base mr-2'>
                             I build applications using TypeScript, React, NextJS, TailwindCSS, Ruby on Rails, and Python.
                         </p>
-                    <div className="my-2 text-base">
+                    <div className="my-2 text-lg">
                         <button 
                             className={`border-2 rounded-lg p-2 hover:scale-110 hover:bg-white hover:text-gunmetal duration-200 cursor-pointer`}
                             type="button"
