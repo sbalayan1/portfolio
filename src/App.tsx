2e574aeabf38c64d2da4e8b760c7538c51dd0cff
sbalayan1 <balayans2014@yahoo.com>
2023-06-29 20:41:41 -0700

    update portfolio component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React, {useState} from 'react';
-// import myImage from './Assets/1547704960267'
-// import pleaseWork from './Assets/logo512.png'
 import NavBar from './Components/NavBar';
 import Home from './Components/Home'
-import Links from './Components/Links'
 import About from './Components/About'
 import Portfolio from './Components/Portfolio';
 import Skills from './Components/Skills';
 import Contact from './Components/Contact';
-// import logo from './logo.svg';
-// import './App.css';
+
 
 function App() {
   const [dark, setDark] = useState(false)
--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -6,7 +6,7 @@ import { useMediaQuery } from 'react-responsive'
 export default function Portfolio() {
     const projectNames = [
         "Gary the Discord Bot", 
-        "RobinhoodUI", 
+        "Robinhood Web Clone", 
         "Portfolio Tracker", 
         "Pokemon Brawl", 
         "NPI Providers Finder"
@@ -18,7 +18,13 @@ export default function Portfolio() {
         "", 
         ""
     ]
-    const imageLinks = ["", "", require('../Assets/PortfolioTracker.png'), require('../Assets/pokemon_brawl.png'), require('../Assets/NPI Provider Finder.png')]
+    const imageLinks = [
+        require('../Assets/discord_bot_1.png'), 
+        require('../Assets/robinhood_web_1.png'), 
+        require('../Assets/PortfolioTracker.png'), 
+        require('../Assets/pokemon_brawl.png'), 
+        require('../Assets/NPI Provider Finder.png')
+    ]
     const githubLinks = [
         "https://github.com/sbalayan1/gary_the_discord_bot", 
         "https://github.com/sbalayan1/tradek.co-landing-page-p1", 
@@ -34,7 +40,7 @@ export default function Portfolio() {
     ]
     const desc = [
         "Gary is a Discord Chat Bot that integrates ChatGPT. It provides dynamic command handling, event listeners for command interactions, and execution of various commands based on user interactions.", 
-        "The RobinhoodUI is a rebuild of Robinhood's Web Application used to build experience with NextJS.", 
+        "The Robinhood Web Clone is a rebuild of Robinhood's Web Application used to build experience with NextJS.", 
         "Portfolio Tracker is an options trading tracker that allows users to track and analyze their options trading as well as view their overall portfolio performance over multiple timeframes.", 
         "Just like old Pokemon Games, PokemonBrawl is a mock Pokemon Battle Application that allows users to catch Pokemon, build and customize teams, and battle other trainers!", 
         "NPI Provider Finder is a full stack application that lets users search the NPI registry and view its results."
@@ -58,7 +64,7 @@ export default function Portfolio() {
             <li key={idx+1} className="h-5/6 mt-4 p-2 md:mt-8 md:mb-12">
                 <div className="h-2/5 w-full mb-10 flex items-center">
                     <a className="p-2 mx-10 hover:scale-110 duration-200" href={obj.app !== "" ? obj.app : obj.demo} target="_blank" rel="noreferrer">
-                        <img className="h-full w-full" src={obj.img} alt={obj.name}/>
+                        <img className="h-5/6 w-5/6" src={obj.img} alt={obj.name}/>
                     </a>
                 </div>
                 <h3 className="text-lg">{obj.name}</h3>
